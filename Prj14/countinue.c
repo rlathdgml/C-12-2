e0ca6cb3b4ff4ddf13a9d021fa0a0b6fd0207e73
rlathdgml <66912211+rlathdgml@users.noreply.github.com>
2020-12-17 17:07:22 +0900

    Add files via upload

---
+++ Prj14/countinue.c
@@ -0,0 +1,18 @@
+// file : countinue.c
+#include <stdio.h>
+
+int main(void)
+{
+	const int MAX = 15;
+
+	printf("1에서 %d가지 정수 중에서 3으로 나누어 떨어지지 않는 수\n", MAX);
+	for (int i = 1; i <= MAX; i++)
+	{
+		if (i % 3 == 0)
+			continue;
+		printf("%3d", i);
+	}
+	puts("");
+
+	return 0;
+}
